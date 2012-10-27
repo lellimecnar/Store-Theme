0caf64d0330d83dab5667116081ac0f7213bf5af
lellimecnar <lellimecnar@gmail.com>
2012-10-27 15:27:36 -0400

    edded theme descriptor

---
+++ theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Store extends Theme {
+
+    public $name            = 'Store Theme';
+    public $author          = 'Lance Miller';
+    public $author_website  = 'http://lellimecnar.com/';
+    public $website         = 'http://lellimecnar.com/';
+    public $description     = 'An HTML5 base template using SASS, Compass, and jQuery.';
+    public $version         = '1.0';
+}
+
+/* End of file theme.php */
